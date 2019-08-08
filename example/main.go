e78de54ae6ced636c6cb1a3a3603d55f095d0a68
Shingo sasaki <shingo.sasaki.0529@gmail.com>
2019-08-08 17:10:04 +0000

    fix typo

--- example/main.go
+++ example/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	fmt.Println(NewClient(
 		os.Getenv("ZAIM_CUSTOMER_KEY"),
-		os.Getenv("ZAIM_CUStOMER_SECRET"),
+		os.Getenv("ZAIM_CUSTOMER_SECRET"),
 		os.Getenv("ZAIM_TOKEN"),
 		os.Getenv("ZAIM_SECRET"),
 	))
