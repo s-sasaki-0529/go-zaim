35c76c2829c2545466a4323adde94f5e4d380aa2
Shingo sasaki <shingo.sasaki.0529@gmail.com>
2019-08-12 08:53:08 +0000

    ユーザ情報を取得できるように

--- API.go
+++ API.go
@@ -2,6 +2,21 @@ package gozaim
 
 import "encoding/json"
 
+func (c *Client) FetchMe() (Me, error) {
+	body, err := c.get("home/user/verify", nil)
+	if err != nil {
+		return Me{}, err
+	}
+
+	var raw struct {
+		Me Me
+		Requested string
+	}
+	json.Unmarshal(body, &raw)
+
+	return raw.Me, nil
+}
+
 func (c *Client) FetchMoney(params map[string]string) ([]Money, error) {
 	body, err := c.get("home/money", params)
 	if err != nil {
--- models.go
+++ models.go
@@ -1,5 +1,21 @@
 package gozaim
 
+type Me struct {
+	ID              int
+	Login           string
+	Name            string
+	InputCount      int
+	DayCount        int
+	RepeatCount     int
+	Day             int
+	Week            int
+	Month           int
+	CurrencyCode    string
+	ProfileImageUrl string
+	CoverImageUrl   string
+	ProfileModified string
+}
+
 type Money struct {
 	ID            int
 	Mode          string
